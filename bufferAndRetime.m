7d9936edddb13f66e223c4acf1cc645fef8bdf0a
Toren Arginteanu <targinteanu@outlook.com>
2024-09-12 01:14:53 -0400

    buffer and fix timing inconsistencies

---
+++ bufferAndRetime.m
@@ -0,0 +1,24 @@
+function [newBuffer, newTail] = bufferAndRetime(oldBuffer, oldTail, newData, bufferFunc)
+% Assume start time of newData is ground truth. Retime oldTail if
+% necessary. 
+
+if nargin < 4
+    bufferFunc = @bufferData; 
+end
+
+t1 = oldTail.Time(end); t2 = newData.Time(1); 
+if t2 <= t1
+    %seconds(t2-t1)
+    T = t2 - oldTail.Time(1);
+    %{
+    oldTail = retime(oldTail,'regular','nearest', ...
+        'TimeStep',T/height(oldTail));
+    %}
+    oldTail.Properties.TimeStep = T/height(oldTail);
+    %seconds(t2-oldTail.Time(end))
+end
+
+newBuffer = bufferFunc(oldBuffer, oldTail); 
+newTail = newData; 
+
+end
--- test_cbmex.m
+++ test_cbmex.m
@@ -1,15 +1,18 @@
 dT = .01; % s between data requests 
-N = round(5/dT); % # of data samples to obtain 
+N = round(3/dT); % # of data samples to obtain 
 channelIndex = 1;
 
 connect_cbmex(); 
 t0 = datetime - seconds(cbmex('time'));
 pause(dT);
 
-D = initRawData_cbmex(channelIndex, 100, t0);
+D = initRawData_cbmex(channelIndex, 60000, t0);
 d = D{1}; 
+d0 = d; d1 = []; d2 = [];
 channelName = d.Properties.VariableNames{1};
-figure; plot(d, channelName); hold on; grid on;
+figure; ax(1) = subplot(2,1,1);
+%plot(d, channelName); 
+hold on; grid on;
 pause(1);
 
 for ti = 1:N
@@ -17,10 +20,18 @@ for ti = 1:N
     D = getNewRawData_cbmex(channelIndex, t0);
     d = D{1}; 
     plot(d, channelName); 
+    d1 = d2; d2 = d;
+    if ~isempty(d1)
+        [d0,d2] = bufferAndRetime(d0, d1, d2);
+    end
     catch ME
     end
     disp(['Progress: ',num2str(100*ti/N,3),'%'])
     pause(dT);
 end
+
+ax(2) = subplot(2,1,2); 
+plot(d0, channelName); grid on; 
+linkaxes(ax, 'xy');
 
 disconnect_cbmex();
