2012f4dcd84845430d869aeedd8c3a8cfd24d0f2
Toren Arginteanu <targinteanu@outlook.com>
2024-09-27 17:30:22 -0400

    Update initFilteredData.m
    rename for clarity

--- initFilteredData.m
+++ initFilteredData.m
@@ -1,4 +1,4 @@
-function filtData = initFilteredData(rawData, filtShift)
+function emptyData = initFilteredData(rawData, filtShift)
 % 
 % Input: 
 %   cell array of unfilt data buffers as timetables 
@@ -8,7 +8,7 @@ function filtData = initFilteredData(rawData, filtShift)
 %   correctly-sized buffers of zero data as timetables 
 % 
 
-filtData = cell(size(rawData));
+emptyData = cell(size(rawData));
 
 for ch = 1:length(rawData)
     rawData_ch = rawData{ch};
@@ -16,13 +16,13 @@ for ch = 1:length(rawData)
     fs = rawData_ch.Properties.UserData.SampleRate;
     ud.SampleRate = fs;
     if bufferSize > filtShift(ch)
-        filtData{ch} = timetable(...
+        emptyData{ch} = timetable(...
             zeros(bufferSize - filtShift(ch), width(rawData_ch)), ...
             'SampleRate', fs, ...
             'StartTime', rawData_ch.Time(1), ...
             'VariableNames', rawData_ch.Properties.VariableNames);
-        filtData{ch}.Properties.UserData = ud;
-        filtData{ch}.Properties.VariableUnits = ...
+        emptyData{ch}.Properties.UserData = ud;
+        emptyData{ch}.Properties.VariableUnits = ...
             rawData_ch.Properties.VariableUnits;
     else
         error(['Data buffer is not long enough to be filtered. ',...
