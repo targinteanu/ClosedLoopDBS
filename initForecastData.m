bdefa31ec19826d7521a14b8f20527d64197768f
Toren Arginteanu <targinteanu@outlook.com>
2024-09-27 17:31:05 -0400

    Update initForecastData.m
    add heads, tails, and buffer

--- initForecastData.m
+++ initForecastData.m
@@ -1,4 +1,4 @@
-function foreData = initForecastData(pastData, foreSamples)
+function [emptyPast, emptyFore, emptyBuff] = initForecastData(pastData, foreSamples)
 % 
 % Input: 
 %   cell array of past data buffers as timetables 
@@ -8,21 +8,25 @@ function foreData = initForecastData(pastData, foreSamples)
 %   correctly-sized buffers of zero data as timetables 
 % 
 
-foreData = cell(size(pastData));
+emptyPast = cellfun(@(T) 0.*T, pastData, 'UniformOutput',false);
+
+emptyFore = cell(size(pastData));
+emptyBuff = emptyFore;
 
 for ch = 1:length(pastData)
     pastData_ch = pastData{ch};
     bufferSize = height(rawData_ch);
     fs = rawData_ch.Properties.UserData.SampleRate;
     ud.SampleRate = fs;
-    foreData{ch} = timetable(...
-        zeros(buffersize + foreSamples(ch), width(pastData_ch)), ...
+    emptyFore{ch} = timetable(...
+        zeros(bufferSize + foreSamples(ch), width(pastData_ch)), ...
         'SampleRate', fs, ...
         'StartTime', pastData_ch.Time(1), ...
         'VariableNames', pastData_ch.Properties.VariableNames); 
-    foreData{ch}.Properties.UserData = ud; 
-    foreData{ch}.Properties.VariableUnits = ...
+    emptyFore{ch}.Properties.UserData = ud; 
+    emptyFore{ch}.Properties.VariableUnits = ...
         pastData_ch.Properties.VariableUnits; 
+    emptyBuff{ch} = bufferData(emptyPast{ch}, emptyFore{ch});
 end
 
 end
