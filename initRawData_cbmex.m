29b57f620c3eed72cee6eb1d883d41eb01a95a9b
Toren Arginteanu <targinteanu@outlook.com>
2024-10-02 15:12:46 -0400

    begin overhauling data structure

--- initRawData_cbmex.m
+++ initRawData_cbmex.m
@@ -1,10 +1,33 @@
-function [emptyData, contData, buffData] = initRawData_cbmex(chsel, bufferSize, initTime)
+function [emptyData, contData, buffData, chanInfo] = ...
+    initRawData_cbmex(chsel, bufferSize)
+% Initialize the multichannel raw data structure using BlackRock cbmex
+% interface. 
+% 
+% Output data structure is a cell array with columns to each channel. Each
+% channel is represented by the matrix [time column, data column]. Time
+% column contains time stamps in machine time seconds and nan elsewhere. 
+% 
+% chanInfo has fields: SampleRate, Name, Unit
+
+%% handle/check inputs 
+if length(bufferSize) < length(chsel)
+    if length(bufferSize) == 1
+        % assume the one input applies to all channels.
+        bufferSize = repmat(bufferSize, size(chsel));
+    else
+        error('Incompatible input dimensions.')
+    end
+end
+
+%% access cbmex 
 
 [spikeEvents, time, continuousData] = cbmex('trialdata',1);
 
 if isempty(continuousData)
     error('No continuous data; ensure that data acquisition has been enabled.')
 end
+
+%% setup 
 
 chnum = [continuousData{:,1}]';
 fs = [continuousData{:,2}]';
@@ -19,6 +42,10 @@ end
 emptyData = cell(1,length(chsel)); 
 contData = emptyData; 
 buffData = emptyData;
+chanInfo = emptyData;
+
+%% assign data to the structure 
+
 for ch = 1:length(chsel)
     chInd = find(chnum == chsel(ch)); 
 
@@ -26,21 +53,11 @@ for ch = 1:length(chsel)
 
         % Create raw data buffer of zeros of the correct length
         L = length(continuousData{chInd,3}); 
-        emptyData{ch} = timetable(...
-            zeros(bufferSize,1), ...
-            'SampleRate', fs(chInd), ...
-            'StartTime', seconds(time - (bufferSize)/fs(chInd)) + initTime, ...
-            'VariableNames', chname(chInd)); 
-        contData{ch} = timetable(...
-            continuousData{chInd,3}, ...
-            'SampleRate', fs(chInd), ...
-            'StartTime', seconds(time) + initTime, ...
-            'VariableNames', chname(chInd));
-
-        % User Data 
-        ud.SampleRate = fs(chInd);
-        emptyData{ch}.Properties.UserData = ud;
-        contData{ch}.Properties.UserData = ud;
+        emptyData{ch} = [nan(bufferSize(ch),1), zeros(bufferSize(ch),1)];
+        %emptyData{ch}(1,1) = time - (bufferSize)/fs(chInd);
+        emptyData{ch}(end,1) = time - 1/fs(chInd); % ??
+        contData{ch} = [nan(L,1), continuousData{chInd,3}];
+        contData{ch}(1,1) = time;
 
         % check units 
         config = cbmex('config', chInd);
@@ -53,8 +70,12 @@ for ch = 1:length(chsel)
             unitname_is = unitname_is(1); 
             unitname = config{unitname_is,2};
         end
-        emptyData{ch}.Properties.VariableUnits = {unitname};
-        contData{ch}.Properties.VariableUnits = {unitname};
+
+        % Channel Info
+        ud.SampleRate = fs(chInd);
+        ud.Name = chname(chInd);
+        ud.Unit = unitname;
+        chanInfo{ch} = ud;
 
         buffData{ch} = bufferData(emptyData{ch}, contData{ch});
 
