64fcdb037151263c6b136cf9a90ec18855fb06ad
Toren Arginteanu <targinteanu@outlook.com>
2024-10-14 14:42:17 -0400

    Update AE_test.m
    unsuccessful attempts to stop the loop process from the next AE call

--- AE_test.m
+++ AE_test.m
@@ -1,11 +1,22 @@
 function AE_test(UserQueue)
 
-afterEach(UserQueue, @myFunc)
+Lst = afterEach(UserQueue, @myFunc);
 
     function myFunc(RecdData)
+        L = evalin('base','Lst');
         DT = string(datetime);
         strOut = "Received data <" + RecdData + "> at time " + DT;
         disp(strOut);
+        if strcmpi(RecdData, "start")
+            iter = 0;
+            while iter < 100
+                pause(.5)
+                iter = iter+1;
+                disp("Process " + num2str(iter) + "% complete...")
+            end
+        elseif strcmpi(RecdData, "stop")
+            delete(L)
+        end
     end
 
 end
