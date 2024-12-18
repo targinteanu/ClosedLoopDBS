4d39f716fee2b780a6ed4f201d85a76e92f3651b
Toren Arginteanu <targinteanu@outlook.com>
2024-12-18 18:12:58 -0500

    Update getRecordedData_NS.m
    simple handling of packet loss (can be improved)

--- getRecordedData_NS.m
+++ getRecordedData_NS.m
@@ -48,26 +48,46 @@ channelIndexStim = find(contains(channelNames, 'ainp1'));
 % obtain usable data variables and other information from the file. 
 
 % Get timing data:
+SamplingFreq = ns.MetaTags.SamplingFreq;
+datalen = ns.MetaTags.DataPoints;
+try
+    t1Rel = ns.MetaTags.Timestamp / ns.MetaTags.TimeRes;
+catch
+    t1Rel = 0;
+end
+if isfield(ns.MetaTags, 'DataDurationSec')
+    datadur = ns.MetaTags.DataDurationSec; 
+elseif isfield(ns.MetaTags, 'DataPointsSec')
+    datadur = ns.MetaTags.DataPointsSec;
+else
+    datadur = datalen/SamplingFreq;
+end
+
+% handle packet loss 
+if (length(t1Rel) > 1) || (length(datadur) > 1)
+    warning('Packet loss. Largest packet will be selected.') % replace with something better?
+    tStartEnd = [t1Rel; t1Rel + datadur];
+    packetdur = diff(tStartEnd);
+    [~,p] = max(packetdur); 
+    dta = ns.Data{p}; datalen = datalen(p); datadur = datadur(p);
+    if length(t1Rel) > 1
+        t1Rel = t1Rel(p);
+    end
+else
+    dta = ns.Data;
+end
+
 % tRel = time relative to start of recording, in seconds 
 % t = absolute date/time 
 % t0 = date/time at start of recording 
-SamplingFreq = ns.MetaTags.SamplingFreq;
-try
-    tRel = linspace(0,ns.MetaTags.DataPointsSec,ns.MetaTags.DataPoints);
-catch
-    try
-        tRel = linspace(0,ns.MetaTags.DataDurationSec,ns.MetaTags.DataPoints);
-    catch
-        tRel = linspace(0,ns.MetaTags.DataPoints/SamplingFreq,ns.MetaTags.DataPoints);
-    end
-end
+tRel = linspace(0, datadur, datalen) + t1Rel;
 t = seconds(tRel);
 t0 = datetime(ns.MetaTags.DateTime); 
 t = t+t0; 
 
 % Interpret data from ns structure: 
-dataAllChannels = double(ns.Data); 
-for ch = 1:height(dataAllChannels) % scale to reported unit 
+dataAllChannels = double(dta); 
+for ch = 1:height(dataAllChannels) % scale to reported unit - can this be replaced with openNSx('uV') ?
     x = dataAllChannels(ch,:); 
     x = x - double( ns.ElectrodesInfo(ch).MinDigiValue ); 
     x = x .* double( ns.ElectrodesInfo(ch).Resolution );
