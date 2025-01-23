3d92de2cd4e601e23ef63e77db82930f7f9e2d82
Toren Arginteanu <targinteanu@outlook.com>
2025-01-23 15:02:17 -0500

    cerestim disconnected warning
    front end warns about connection issue to avoid clogging up the backend

--- GUI_PD_v1.m
+++ GUI_PD_v1.m
@@ -1671,6 +1671,20 @@ if get(hObject, 'Value') == 1
 
     try
 
+    % check cerestim connection before proceeding 
+    stimulator = cerestim96();
+    dl = stimulator.scanForDevices();
+    if isempty(dl)
+        warnmsg = ['Difficulty connecting to CereStim. ' ...
+            'It is recommended to turn the device off and on ' ...
+            'again before proceeding.'];
+        resp = questdlg(warnmsg, 'CereStim connection issue', ...
+            'Do Not Proceed', 'Proceed Anyway', 'Do Not Proceed');
+        if ~strcmp(resp, 'Proceed Anyway')
+            error('Try again after turning the CereStim off and on.')
+        end
+    end
+
     handles.stimMaxFreq = eval(get(handles.txt_MaxStimFreq, 'String'));
 
     handles.StimActive = true;
