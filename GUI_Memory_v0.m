5590fe4b929b4fc9ca71d2c98b04b0dd033e4f8d
Toren Arginteanu <targinteanu@outlook.com>
2025-06-18 16:51:31 -0400

    Update GUI_Memory_v0.m
    fix artifact removal timing issues: no more cutoff, and start time now reflects stimulator lag

--- GUI_Memory_v0.m
+++ GUI_Memory_v0.m
@@ -86,7 +86,8 @@ end
 ud = struct('ReceivedData', '', ...
             'TrialNumber', -1, ...
             'StimOn', false, ...
-            'ParadigmPhase', 'WAIT', ...
+            ...'ParadigmPhase', 'WAIT', ...
+            'ParadigmPhase', 'HOLD', ...
             'ImageVisible', false);
 receiverSerial.UserData = ud;
 if ~noSerialSetup
@@ -105,6 +106,7 @@ handles.showElecGrid = false;
 handles.srlLastMsg  = ud.ReceivedData;
 handles.stimLastTime = -inf;
 handles.stimNewTime = -inf;
+handles.artReplaceRemaining = [];
 
 emptyStorage = nan(100000,1);
 handles.pkStorage1 = emptyStorage; handles.pkP1 = 1;
@@ -397,6 +399,22 @@ try
         % as specified in UI *****
         
     newContinuousData = continuousData{handles.channelIndex,3}; 
+
+    % artifact removal (1)
+    if handles.FilterSetUp
+    if handles.MdlSetUp
+    if handles.check_artifact.Value
+        if numel(handles.artReplaceRemaining)
+            % continue replacing from last loop iter
+            artLen = min(length(handles.artReplaceRemaining), length(newContinuousData));
+            artReplaceRemaining = handles.artReplaceRemaining(1:artLen); 
+            handles.artReplaceRemaining = handles.artReplaceRemaining((artLen+1):end);
+            newContinuousData(1:artLen) = artReplaceRemaining;
+        end
+    end
+    end
+    end
+
     handles.rawDataBuffer = cycleBuffer(handles.rawDataBuffer, newContinuousData);
     N = length(newContinuousData);
     t0 = handles.lastSampleProcTime; 
@@ -530,14 +548,15 @@ try
             if stimind > 0
                 handles.stimDataBuffer(stimind) = true;
 
-                % artifact removal 
+                % artifact removal (2)
                 if handles.check_artifact.Value
                     try
                     artInd = stimind - N; 
                     artInd = (-5:24) + artInd ... % set artifact duration 
-                        + round(handles.StimulatorLagTime/handles.fSample);
-                    artInd = artInd(artInd > 0);
-                    artInd = artInd(artInd <= handles.bufferSize); % !!!!!
+                        + round(handles.StimulatorLagTime*handles.fSample);
+                    artInd = artInd(artInd > 0); % can't change the past
+                    artLen = length(artInd);
+                    artInd = artInd(artInd <= handles.bufferSize); 
                     artPastData = zeros(handles.PDSwin1,1); 
                     if ~isempty(artInd)
                     artPastStart = artInd(1) - handles.PDSwin1;
@@ -546,9 +565,12 @@ try
                     artPastData1 = handles.rawDataBuffer(artPastStart:(artInd(1)-1),:);
                     artPastData1 = artPastData1 - rawOffset;
                     artPastN = size(artPastData1,1);
-                    artPastData((end-artPastN+1):end,:) = artPastData1;
-                    artReplace = myFastForecastAR(handles.Mdl, artPastData, length(artInd));
+                    artPastData((end-artPastN+1):end,:) = artPastData1; 
+                    artReplace = myFastForecastAR(handles.Mdl, artPastData, artLen);
                     artReplace = artReplace + rawOffset;
+                    handles.artReplaceRemaining = artReplace((length(artInd)+1):end); 
+                        % continue replacing on next loop iter
+                    artReplace = artReplace(1:length(artInd));
                     handles.rawDataBuffer(artInd) = artReplace;
                     end
                     catch ME3 
