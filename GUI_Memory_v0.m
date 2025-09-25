ee0c7ab77ffebf1c4103495515c938456afe98aa
Toren Arginteanu <targinteanu@outlook.com>
2025-09-24 23:30:12 -0400

    serial callback must be specific to paradigm type!

--- GUI_Memory_v0.m
+++ GUI_Memory_v0.m
@@ -87,6 +87,8 @@ svloc = ['Saved Data Memory',filesep,'Saved Data ',...
 pause(1)
 mkdir(svloc); 
 
+% ***** REPLACE BELOW: function should take in CharSerialCallback := 
+% @(hsrl, hevt) CharSerialCallbackReceiver_Memory_v0(hsrl, hevt, handles.textSrl, handles.ParadigmInfoTable)
 handles = helperGUIv0_OpeningInitialize(handles, ud, svloc);
 
 % additional phase tracking buffers & objects
