7fe19ec52aab095fde630a70318bd0baf6044166
Toren Arginteanu <targinteanu@outlook.com>
2026-01-08 15:30:53 -0500

    break out myPULSE
    break out new function for readability and shared function between v1b GUIs. Planned addition for artifact removal - not yet implemented

--- GUI_PD_v1b.m
+++ GUI_PD_v1b.m
@@ -771,31 +771,7 @@ keyboard
 
 function myPULSE(hTimer,eventdata,hFigure)
 handles = guidata(hFigure);
-initTic = handles.initTic;
-stimtime1 = handles.HardwareFuncs.GetTime(initTic);
-handles.stimulator = handles.HardwareFuncs.PulseStimulator(handles.stimulator);
-stimtime2 = handles.HardwareFuncs.GetTime(initTic);
-% disp time of pulse using eventdata
-eventTime = datestr(eventdata.Data.time);
-stimtime = .5*(stimtime1 + stimtime2);
-dstimtime = stimtime2 - stimtime1; 
-stimschedtime = hTimer.UserData; 
-disp(['Stimulus pulsed at ',eventTime,' within ',num2str(dstimtime),'s, ',...
-      num2str(stimtime - stimschedtime),' s late'])
-handles.stimLastTime = stimtime; handles.stimNewTime = stimtime; 
-if handles.stP1 <= length(handles.stStorage1)
-    handles.stStorage1(handles.stP1) = stimtime; 
-    handles.stP1 = handles.stP1 + 1;
-else
-    % storage full; save
-    StimTime = handles.stStorage1;
-    svfn = [handles.SaveFileLoc,filesep,'SaveFile',num2str(handles.SaveFileN),'.mat'];
-    disp(['Saving Stimulus to ',svfn])
-    save(svfn,'StimTime');
-    handles.SaveFileN = handles.SaveFileN + 1;
-    handles.stP1 = 1;
-    handles.stStorage1 = nan(size(handles.stStorage1));
-end
+handles = helperGUIv1b_myPULSE(hTimer, eventdata, handles);
 guidata(hFigure,handles);
 
 function schedulePULSE(hTimer,eventdata,hFigure)
